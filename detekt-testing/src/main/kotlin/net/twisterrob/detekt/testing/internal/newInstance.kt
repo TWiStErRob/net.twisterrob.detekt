a2ae23d9bbff514e0d41e47bde79abe387c10e71
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-28 10:35:24 +0000

    BaseRule is now Rule.

--- detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/internal/newInstance.kt
+++ detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/internal/newInstance.kt
@@ -1,14 +1,14 @@
 package net.twisterrob.detekt.testing.internal
 
-import io.gitlab.arturbosch.detekt.api.BaseRule
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Rule
 import javax.annotation.CheckReturnValue
 import kotlin.reflect.KClass
 
 @CheckReturnValue
 @Deprecated("Do not use this method, only visible so that inlining works.", level = DeprecationLevel.ERROR)
 @Suppress("detekt.UndocumentedPublicFunction") // Only public because of inlining.
-public fun <T : BaseRule> KClass<T>.newInstance(config: Config): T {
+public fun <T : Rule> KClass<T>.newInstance(config: Config): T {
 	val constructor =
 		this.constructors.singleOrNull { it.parameters.size == 1 && it.parameters[0].type.classifier == Config::class }
 			?: error("Rule ${this} should have a constructor where Config is the only parameter.")
