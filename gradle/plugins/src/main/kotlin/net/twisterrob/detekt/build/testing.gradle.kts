fd4565c84e17b7f02d8058315f0be757dbf9e40c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-03 00:49:20 +0100

    test range of java

--- gradle/plugins/src/main/kotlin/net/twisterrob/detekt/build/testing.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/detekt/build/testing.gradle.kts
@@ -14,11 +14,26 @@ testing.suites.withType<JvmTestSuite>().configureEach {
 		implementation(project(":test-helpers"))
 	}
 
-	targets.configureEach {
-		testTask.configure {
-			javaLauncher = javaToolchains.launcherFor {
-				languageVersion = libs.versions.java.test.map(JavaLanguageVersion::of)
+	targets {
+		val minJava = libs.versions.java.target.map(String::toInt).get()
+		val maxJava = libs.versions.java.compile.map(String::toInt).get()
+		named(name).configure {
+			testTask.configure {
+				// javaLauncher = <not set, inherited from Gradle, see java-compile.
+				description = "Runs the test suite on JDK ${maxJava}"
 			}
 		}
+		listOf(minJava) // Not (minJava..<maxJava) to save time.
+			.map(JavaLanguageVersion::of)
+			.forEach { javaVersion ->
+				register("testJdk${javaVersion.asInt()}") {
+					testTask.configure {
+						description = "Runs the test suite on JDK ${javaVersion}"
+						javaLauncher = javaToolchains.launcherFor {
+							languageVersion = javaVersion
+						}
+					}
+				}
+			}
 	}
 }
