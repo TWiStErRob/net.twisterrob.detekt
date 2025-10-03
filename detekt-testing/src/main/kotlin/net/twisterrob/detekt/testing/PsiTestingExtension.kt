c21153af168c752fc720f16b7cbd85780fae9844
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-03 02:04:09 +0100

    org.jetbrains.kotlin.com.intellij.psi.impl.DebugUtil.CHECK is gone, removed prefix

--- detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/PsiTestingExtension.kt
+++ detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/PsiTestingExtension.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
 /**
- * JUnit Jupiter extension that enables [org.jetbrains.kotlin.com.intellij.psi.impl.DebugUtil.CHECK] for each test.
+ * JUnit Jupiter extension that enables [com.intellij.psi.impl.DebugUtil.CHECK] for each test.
  *
  * This ensures that the potential modifications that are done to the PSI tree are valid.
  *
@@ -14,10 +14,10 @@ import org.junit.jupiter.api.extension.ExtensionContext
 public class PsiTestingExtension : BeforeEachCallback, AfterEachCallback {
 
 	override fun beforeEach(context: ExtensionContext) {
-		org.jetbrains.kotlin.com.intellij.psi.impl.DebugUtil.CHECK = true
+		com.intellij.psi.impl.DebugUtil.CHECK = true
 	}
 
 	override fun afterEach(context: ExtensionContext) {
-		org.jetbrains.kotlin.com.intellij.psi.impl.DebugUtil.CHECK = false
+		com.intellij.psi.impl.DebugUtil.CHECK = false
 	}
 }
