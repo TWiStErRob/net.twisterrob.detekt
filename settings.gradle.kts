daa360e4c16b66656d77bd7224684fbea79b6bef
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-03 02:01:58 +0100

    Remove nagging suppression

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-import net.twisterrob.gradle.doNotNagAbout
 import net.twisterrob.gradle.settings.enableFeaturePreviewQuietly
 
 // TODEL https://github.com/gradle/gradle/issues/18971
@@ -22,7 +21,6 @@ pluginManagement {
 
 plugins {
 	id("net.twisterrob.gradle.plugin.settings") version "0.18"
-	id("net.twisterrob.gradle.plugin.nagging") version "0.18"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
@@ -45,16 +43,3 @@ include(":detekt-testing")
 
 include(":detekt-browser")
 include(":test-helpers")
-
-val gradleVersion: String = GradleVersion.current().version
-
-// TODEL Gradle 9.1 vs detekt 1.23.8 https://github.com/detekt/detekt/issues/8452
-@Suppress("detekt.MaxLineLength")
-doNotNagAbout(
-	"The ReportingExtension.file(String) method has been deprecated. " +
-			"This is scheduled to be removed in Gradle 10. " +
-			"Please use the getBaseDirectory().file(String) or getBaseDirectory().dir(String) method instead. " +
-			"Consult the upgrading guide for further information: " +
-			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_9.html#reporting_extension_file",
-	"at io.gitlab.arturbosch.detekt.DetektPlugin.apply(DetektPlugin.kt:28)",
-)
