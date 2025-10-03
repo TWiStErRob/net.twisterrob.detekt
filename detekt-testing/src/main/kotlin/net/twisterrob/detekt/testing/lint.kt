76fadf700e48e287e1dfbb50c0bcd3b1e78604a1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-03 02:04:20 +0100

    compileAndLint -> lint?

--- detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/lint.kt
+++ detekt-testing/src/main/kotlin/net/twisterrob/detekt/testing/lint.kt
@@ -3,7 +3,7 @@ package net.twisterrob.detekt.testing
 import dev.detekt.api.Config
 import dev.detekt.api.Finding
 import dev.detekt.api.Rule
-import dev.detekt.test.compileAndLint
+import dev.detekt.test.lint
 import net.twisterrob.detekt.testing.internal.newInstance
 import org.intellij.lang.annotations.Language
 import javax.annotation.CheckReturnValue
@@ -18,5 +18,5 @@ public inline fun <reified T : Rule> lint(
 ): List<Finding> {
 	@Suppress("DEPRECATION_ERROR")
 	val rule = T::class.newInstance(config)
-	return rule.compileAndLint(originalCode)
+	return rule.lint(originalCode)
 }
